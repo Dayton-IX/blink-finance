f8fdafab17be18f81b2ee35ce647e9119351ebae
Max Dayton <daytonmaximus@gmail.com>
2021-06-22 01:28:04 -0600

    number formatted input field, input not working

--- components/modals/NewTransactionModal.tsx
+++ components/modals/NewTransactionModal.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import Modal from "../micro/Modal";
+import NumberFormat from "react-number-format"
 
 type Props = {
     open: boolean
@@ -13,16 +14,18 @@ enum TransactionType {
 
 const NewTransactionModal = ({open, onClose}: Props) => {
     const [type, setType] = useState(TransactionType.INCOME)
+    const [transactionValue, setTransactionValue] = useState<number | undefined>(0)
+
     return (
         <Modal open={open} onClose={() => onClose()}>
             <h1 className="text-accentSecondary text-2xl">New Transaction</h1>
-            <div className="mx-auto flex flex-row justify-center mt-12" >
+            <div className="mx-auto flex flex-col justify-center mt-12" >
                 <div className="mx-auto flex flex-row justify-between">
                     <button onClick={() => setType(TransactionType.INCOME)} className={`${type === TransactionType.INCOME ? 'bg-gradient-to-br from-accentMain to-accentSecondary text-dark-textMain' : 'text-accentSecondary ring-accentSecondary ring-inset ring-2'} transition-all rounded-md px-4 py-2 mx-3 focus:outline-none`}>+ Income</button>
                     <button onClick={() => setType(TransactionType.EXPENSE)} className={`${type === TransactionType.EXPENSE ? 'bg-gradient-to-tr from-redLight to-redMain text-dark-textMain' : 'text-redLight ring-redLight ring-inset ring-2'} transition-all rounded-md px-4 py-2 mx-3 focus:outline-none`}>- Expense</button>
                 </div>
+                <NumberFormat value={transactionValue} onValueChange={(values) => setTransactionValue(values.floatValue)} className={`flex flex-row items-center justify-center mx-auto p-5 focus:outline-none ${type === TransactionType.INCOME ? "text-accentMain" : "text-redLight"} text-4xl w-5/6 mt-10 bg-transparent text-center transition-all`} allowNegative={false} decimalScale={2} fixedDecimalScale thousandSeparator allowEmptyFormatting placeholder="0.00" format={`${type === TransactionType.INCOME ? '+' : '-'}$`} />
             </div>
-
         </Modal>
     )
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,29 +3,30 @@ module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: 'class', // or 'media' or 'class'
   theme: {
-    colors: {
-      accentMain: '#76BB86',
-      accentSecondary: '#B0DAB9',
-      redMain: '#BC4646',
-      redLight: '#D66565',
-      dark: {
-          bgMain: '#1E2327',
-          bgSecondary: '#16191C',
-          bgTertiary: '#282E34',
-          textMain: '#FFFFFF',
-          textSecondary: '#DEDEDE',
-          textDark: '#606060'
+    extend: {
+      colors: {
+        accentMain: '#76BB86',
+        accentSecondary: '#B0DAB9',
+        redMain: '#BC4646',
+        redLight: '#D66565',
+        dark: {
+            bgMain: '#1E2327',
+            bgSecondary: '#16191C',
+            bgTertiary: '#282E34',
+            textMain: '#FFFFFF',
+            textSecondary: '#DEDEDE',
+            textDark: '#606060'
+        },
+        light: {
+            bgMain: '#1E2327',
+            bgSecondary: '#16191C',
+            bgTertiary: '#282E34',
+            textMain: '#FFFFFF',
+            textSecondary: '#DEDEDE',
+            textDark: '#606060'
+        },
       },
-      light: {
-          bgMain: '#1E2327',
-          bgSecondary: '#16191C',
-          bgTertiary: '#282E34',
-          textMain: '#FFFFFF',
-          textSecondary: '#DEDEDE',
-          textDark: '#606060'
-      },
-    },
-    extend: {},
+    }
   },
   variants: {
     extend: {
