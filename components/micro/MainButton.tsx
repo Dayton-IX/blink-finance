41b631f44f771c3fbd768f45a004d36e378809fd
Max Dayton <daytonmaximus@gmail.com>
2021-06-26 16:49:23 -0600

    color changes

--- components/micro/MainButton.tsx
+++ components/micro/MainButton.tsx
@@ -4,15 +4,17 @@ import { Logo } from '../../constants/svgLib';
 type Props = {
     className?: string,
     style?: object,
+    colorA?: string,
+    colorB?: string,
     children: React.ReactChild,
     onClick: Function,
     disabled?: boolean,
     loading?: boolean
 }
 
-const MainButton = ({ className, style, children, onClick, disabled=false, loading=false }: Props) => {
+const MainButton = ({ className, style, colorA="accentMain", colorB="accentSecondary", children, onClick, disabled=false, loading=false }: Props) => {
     return (
-        <button disabled={disabled} onClick={() => onClick()} className={`${className} text-light-textMain text-lg p-3 bg-gradient-to-br from-accentMain to-accentSecondary my-2 flex flex-col justify-between rounded-lg transition-all focus:ring-2 focus:outline-none ring-accentSecondary hover:ring-1`} style={style}>
+        <button disabled={disabled} onClick={() => onClick()} className={`${className} text-light-textMain text-lg p-3 bg-gradient-to-br from-${colorA} to-${colorB} my-2 flex flex-col justify-between rounded-lg transition-all focus:ring-2 focus:outline-none ring-accentSecondary hover:ring-1`} style={style}>
             {loading ? <Logo className="animate-spin mr-2" color={Colors.dark.textMain} /> : null}
             {children}
         </button>
