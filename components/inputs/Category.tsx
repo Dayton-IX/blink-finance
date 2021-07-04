80b5995d754020e9d536572bf9c4c89109659c1b
Max Dayton <daytonmaximus@gmail.com>
2021-07-03 22:38:19 -0700

    category component file and map loop

---
+++ components/inputs/Category.tsx
@@ -0,0 +1,13 @@
+type Props = {
+    category: Category
+}
+
+const Category = ({category}: Props) => {
+    return (
+        <div className="flex flex-row w-11/12 mx-auto">
+            
+        </div>
+    )
+};
+
+export default Category;
--- components/modals/EditCategoriesModal.tsx
+++ components/modals/EditCategoriesModal.tsx
@@ -1,4 +1,6 @@
+import { useState } from 'react'
 import Modal from "../micro/Modal";
+import Category from "../inputs/Category"
 
 type Props = {
     open: boolean,
@@ -6,11 +8,45 @@ type Props = {
 }
 
 const EditCategories = ({open, onClose}: Props) => {
+    const [categories, setCategories] = useState<Category[]>([
+        {
+			id: '1',
+			name: 'Rent',
+			monthlyAmount: 1650,
+			remainingAmount: 0
+		},
+		{
+			id: '2',
+			name: 'Food',
+			monthlyAmount: 400,
+			remainingAmount: 230
+		},
+		{
+			id: '3',
+			name: 'Gas',
+			monthlyAmount: 50,
+			remainingAmount: 20
+		},
+		{
+			id: '4',
+			name: 'Investments',
+			monthlyAmount: 50,
+			remainingAmount: 20
+		},
+		{
+			id: '5',
+			name: 'Other',
+			monthlyAmount: 100,
+			remainingAmount: 30
+		},
+    ])
     return (
         <Modal open={open} onClose={() => onClose()}>
             <h1 className="text-accentSecondary text-2xl">Categories</h1>
             <div className="flex flex-col w-11/12 mx-auto">
-
+                {categories.map((category, input) => (
+                    <Category category={category} />
+                ))}
             </div>
         </Modal>
     )
